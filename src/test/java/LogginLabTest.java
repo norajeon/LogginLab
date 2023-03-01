3aade90cb63c28c8396c73679a58d7a6ef61beb4
Nora <nora@Zipcoders-MacBook-Pro-51.local>
2023-03-01 13:53:57 -0500

    Answer for LogginLab

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -29,6 +29,26 @@ public class LogginLabTest {
                 logger.log(Level.INFO, "Threshold finally reached!");
                 assertFalse(lab.thresholdExceeds(i));
             }
+
+        }
+    }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i <= finalLimit; i++) {
+            if (!lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                assertFalse(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold finally reached!");
+                assertTrue(lab.thresholdReached(i));
+            }
+
         }
     }
 }
